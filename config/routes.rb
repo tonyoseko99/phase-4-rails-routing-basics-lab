d436dd21c9fbc7830c600569a6dce4d5034c80b0
Tonny Oseko <tonnyoseko0@gmail.com>
2022-09-26 23:15:31 +0300

    commit tree 290c9d0120dc035552c078c59935a963615b8255

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # route to students
+  get '/students', to: 'students#index'
+
+  # route to students/grades
+  get '/students/grades', to: 'students#grades'
+
+  # route to students highest grade
+  get '/students/highest-grade', to: 'students#highest_grade'
 end
